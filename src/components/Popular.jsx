bbdf6fd9f16b272a30fe98f8c4d97a1ee7bad24a
Mark Dugay <mdugay4@gmail.com>
2022-09-10 18:04:15 -0700

    first commit

---
+++ src/components/Popular.jsx
@@ -0,0 +1,74 @@
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+import '@splidejs/react-splide/css';
+
+function Popular() {
+    const [popular, setPopular] = useState([]);
+
+    useEffect(() => {
+        getPopular();
+    }, []);
+
+    const getPopular = async () => {
+        const api = await fetch(
+            `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
+        );
+        const data = await api.json();
+        console.log(data);
+        setPopular(data.recipes);
+    };
+
+    return (
+        <div>
+            <Wrapper>
+                <h3>Popular Picks</h3>
+                <Splide
+                    options={{
+                        perPage: 4,
+                        arrows: false,
+                        pagination: false,
+                        drag: 'free',
+                        gap: '5rem',
+                    }}
+                >
+                    {popular.map((recipe) => {
+                        return (
+                            <SplideSlide>
+                                <Card>
+                                    <p key={recipe.id}>{recipe.title}</p>
+                                    <img
+                                        src={recipe.image}
+                                        alt={recipe.title}
+                                    />
+                                </Card>
+                            </SplideSlide>
+                        );
+                    })}
+                </Splide>
+            </Wrapper>
+            );
+        </div>
+    );
+}
+
+const Wrapper = styled.div`
+    margin: 4rem 0rem;
+`;
+
+const Card = styled.div`
+    min-height: 25rem;
+    border-radius: 2rem;
+    overflow: hidden;
+    position: relative;
+    img {
+        border-radius: 2rem;
+        position: absolute;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+`;
+
+export default Popular;
