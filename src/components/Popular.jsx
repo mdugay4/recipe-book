de02464ae1a8d1069f20cecba58e4d007c63694c
Mark Dugay <mdugay4@gmail.com>
2022-09-11 14:07:43 -0700

    API Fetch Update

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -11,12 +11,20 @@ function Popular() {
     }, []);
 
     const getPopular = async () => {
-        const api = await fetch(
-            `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
-        );
-        const data = await api.json();
-        console.log(data);
-        setPopular(data.recipes);
+        const check = localStorage.getItem('popular');
+
+        if (check) {
+            setPopular(JSON.parse(check));
+        } else {
+            const api = await fetch(
+                `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
+            );
+            const data = await api.json();
+
+            localStorage.setItem('popular', JSON.stringify(data.recipes));
+            console.log(data.recipes);
+            setPopular(data.recipes);
+        }
     };
 
     return (
@@ -41,6 +49,7 @@ function Popular() {
                                         src={recipe.image}
                                         alt={recipe.title}
                                     />
+                                    <Gradient />
                                 </Card>
                             </SplideSlide>
                         );
@@ -69,6 +78,30 @@ const Card = styled.div`
         height: 100%;
         object-fit: cover;
     }
+    p {
+        position: absolute;
+        z-index: 10;
+        left: 50%;
+        bottom: 0%;
+        transform: translate(-50%, 0%);
+        color: white;
+        width: 100%;
+        text-align: center;
+        font-weight: 600;
+        font-size: 1rem;
+        height: 40%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+`;
+
+const Gradient = styled.div`
+    z-index: 3;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5));
 `;
 
 export default Popular;
