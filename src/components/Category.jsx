a2eb56f9e8206082d8411e376f4e1fb62e585647
Mark Dugay <mdugay4@gmail.com>
2022-09-11 17:30:45 -0700

    Added Specific Food Pages

---
+++ src/components/Category.jsx
@@ -0,0 +1,35 @@
+import { FaPizzaSlice, FaHamburger } from 'react-icons/fa';
+import { GiNoodles, GiChopsticks } from 'react-icons/gi';
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+
+function Category() {
+    return (
+        <List>
+            <NavLink to={'/cuisine/Italian'}>
+                <FaPizzaSlice />
+                <h4>Italian</h4>
+            </NavLink>
+            <NavLink to={'/cuisine/American'}>
+                <FaHamburger />
+                <h4>American</h4>
+            </NavLink>
+            <NavLink to={'/cuisine/Thai'}>
+                <GiNoodles />
+                <h4>Thai</h4>
+            </NavLink>
+            <NavLink to={'/cuisine/Korean'}>
+                <GiChopsticks />
+                <h4>Korean</h4>
+            </NavLink>
+        </List>
+    );
+}
+
+const List = styled.div`
+    display: flex;
+    justify-content: center;
+    margin: 2rem 0rem;
+`;
+
+export default Category;
